533665f434bfff8c2883467f03a73b9f8a90651b
Ross <jonathan.ross@adesso.de>
2023-07-07 07:55:23 +0200

    Not buildable. Removed all unnecessary Classes.

--- src/main/java/de/adesso/trmdeamon/repository/BookingAttributesRepository.java
+++
@@ -1,7 +0,0 @@
-package de.adesso.trmdeamon.repository;
-
-import de.adesso.trmdeamon.model.BookingAttribute;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BookingAttributesRepository extends JpaRepository<BookingAttribute, Long> {
-}
--- src/main/java/de/adesso/trmdeamon/repository/BucketAttributesRepository.java
+++
@@ -1,7 +0,0 @@
-package de.adesso.trmdeamon.repository;
-
-import de.adesso.trmdeamon.model.BucketAttribute;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BucketAttributesRepository extends JpaRepository<BucketAttribute, Long> {
-}
