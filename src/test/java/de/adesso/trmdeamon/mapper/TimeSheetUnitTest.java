ec6b6385a4f4ec30430fbdb91e3b490f79df0f6f
Ross <jonathan.ross@adesso.de>
2023-07-21 14:50:24 +0200

    Fixed Tests.

--- src/test/java/de/adesso/trmdeamon/mapper/TimeSheetUnitTest.java
+++ src/test/java/de/adesso/trmdeamon/mapper/TimeSheetUnitTest.java
@@ -1,11 +1,14 @@
 package de.adesso.trmdeamon.mapper;
 
 import de.adesso.trmdeamon.dto.timesheet.TimeSheetReadDto;
+import de.adesso.trmdeamon.model.Setting;
 import de.adesso.trmdeamon.model.TimeSheet;
 import org.hamcrest.MatcherAssert;
 import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Spy;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.ArrayList;
@@ -16,7 +19,15 @@ import static org.hamcrest.Matchers.equalTo;
 @ExtendWith(MockitoExtension.class)
 public class TimeSheetUnitTest {
 
-    private TimeSheetMapper mapper;
+    @Spy
+    private TagMapper tagMapper = new TagMapper();
+    @Spy
+    private BookingMapper bookingMapper = new BookingMapper(tagMapper);
+    @Spy
+    private SettingsMapper settingsMapper = new SettingsMapper();
+
+    @InjectMocks
+    private TimeSheetMapper cut;
 
     @Test
     public void testToReadDto() {
@@ -28,7 +39,7 @@ public class TimeSheetUnitTest {
                 .bookings(new ArrayList<>())
                 .settings(new ArrayList<>())
                 .build();
-        TimeSheetReadDto dto = mapper.toReadDto(ts);
+        TimeSheetReadDto dto = cut.toReadDto(ts);
         MatcherAssert.assertThat(dto, Matchers.allOf(
                 Matchers.hasProperty("id", equalTo(id)),
                 Matchers.hasProperty("name", equalTo(name))
